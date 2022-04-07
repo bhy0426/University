7a1a9e520d7f64739b8e91a1fcc3adede04fc91d
qusgkdus <bhy246@naver.com>
2022-04-07 15:06:55 +0900

    20220407_lap

---
+++ 대학 강의/컴퓨터프로그래밍기초 I (김국현)/dev/hello/hello/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World!\n");
+	printf("Kim ChulSoo\n");
+
+	printf("3 X 1 = 3\n");
+	printf("3 X 2 = 6\n");
+	printf("3 X 3 = 9\n");
+
+	printf("2+5=%d\n", 2 + 5);
+	printf("2-3=%d\n", 2 - 3);
+	printf("2*3=%d\n", 2 * 3);
+	printf("2/3=%d\n", 2 / 3);
+	return 0;
+}
